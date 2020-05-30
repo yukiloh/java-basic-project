30c7bcbefbb98e23718f229204ff509838a35271
Ash <yukiloh001@gmail.com>
2020-05-31 05:54:59 +0800

    update

---
+++ Module-01-JavaSE/src/main/java/Thread/ThreadTest.java
@@ -0,0 +1,82 @@
+package Thread;
+
+import org.junit.jupiter.api.Test;
+
+public class ThreadTest {
+
+
+    @Test
+    void testContext() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("111");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("222");
+                }
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("333");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread2.join();
+        thread3.start();
+    }
+
+
+
+    //测试sleep(0)
+    @Test
+    void testContext1() throws InterruptedException {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("111");
+                    try {
+                        Thread.sleep(0);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    System.out.println("222");
+                    try {
+                        Thread.sleep(0);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+
+
+}
